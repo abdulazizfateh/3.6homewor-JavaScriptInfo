78ec8ff3494f58e2636c2b57d6a3f6b673ff71c1
abdulazizfateh <eabdulaziz36@gmail.com>
2025-02-23 01:57:51 +0500

    Added JS Problems

---
+++ js/main.js
@@ -0,0 +1,83 @@
+// if1. Butun son berilgan. Agar, berilgan son musbat bolsa, 1 ga oshirilsin, aks holda o zgartirilmasin.
+// Hosil bo'lgan sonni ekranga chiqaruvchi programma tuzilsin.
+// if2. Butun son berilgan. Agar, berilgan son musbat bo'lsa, 1 ga oshiring, aks holda 2 ga kamaytiring.
+// Hosil bo lgan sonni ekranga chiqaruvchi programma tuzilsin.
+// if3. Butun son berilgan. Agar, berilgan son musbat bolsa, 1 ga oshiring, agar manfiy bo lsa 2 ga kamaytiring. Agar O ga teng bo'lsa, 10 ni o zlashtirsin. Hosil bolgan sonni ekranga chiqaruvchi programma tuzilsin.
+// if4. Uchta butun son berilgan. Shu sonlar orasidan nechta musbat son borligini aniqlovchi programma tuzilsin.
+// if5. Uchta butun son berilgan. Shu sonlar orasidan nechta musbat va manfiy son borligini aniglovchi programma tuzilsin.
+// if6. kkita butun son berilgan. Shu sonlaming kattasini aniqlovchi programma tuzilsin.
+
+
+// TASK 1
+// let a = Number(prompt("Butun son kiriting"));
+// if (a > 0) {
+//     console.log(a + 1);
+// } else {
+//     console.log(a);
+// }
+
+// TASK 2
+// let a = Number(prompt("Butun son kiriting"));
+// if (a > 0) {
+//     console.log(a + 1);
+// } else {
+//     console.log(a - 2);
+// }
+
+// TASK 3
+// let a = Number(prompt("Butun son kiriting"));
+// if (a > 0) {
+//     console.log(a + 1);
+// } else if (a == 0) {
+//     a = 10;
+//     console.log(a);
+// } else {
+//     console.log(a - 2);
+// }
+
+// TASK 4
+// let a = Number(prompt("Birinchi sonni kiriting"));
+// let b = Number(prompt("Ikkinchi sonni kiriting"));
+// let c = Number(prompt("Uchinchi sonni kiriting"));
+// let count = 0;
+// if (a > 0) {
+//     count = count + 1;
+// } if (b > 0) {
+//     count = count + 1;
+// } if (c > 0) {
+//     count = count + 1;
+// }
+// console.log(count);
+
+// TASK 5
+// let a = Number(prompt("Birinchi sonni kiriting"));
+// let b = Number(prompt("Ikkinchi sonni kiriting"));
+// let c = Number(prompt("Uchinchi sonni kiriting"));
+// let countP = 0;
+// let countN = 0;
+// if (a > 0) {
+//     countP = countP + 1;
+// } else {
+//     countN = countN + 1;
+// }
+// if (b > 0) {
+//     countP = countP + 1;
+// } else {
+//     countN = countN + 1;
+// }
+// if (c > 0) {
+//     countP = countP + 1;
+// } else {
+//     countN = countN + 1;
+// }
+// console.log(`Umumiy musbat sonlar:  ${countP}`);
+// console.log(`Umumiy manfiy sonlar:  ${countN}`);
+
+//TASK 6
+// let a = Number(prompt("Birinchi sonni kiriting"));
+// let b = Number(prompt("Ikkinchi sonni kiriting"));
+// if (a > b) {
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
